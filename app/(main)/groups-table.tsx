be49b5b03019c08a10acdd0a472d770fd35bf133
ReiAC <lhh@live.com>
2025-09-04 22:28:18 +0800

    feat: 添加Tab页描述

--- app/(main)/groups-table.tsx
+++ app/(main)/groups-table.tsx
@@ -1,33 +1,17 @@
 'use client';
 
-import {
-  TableHead,
-  TableRow,
-  TableHeader,
-  TableBody,
-  Table
-} from '@/components/ui/table';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card';
+import { TableHead, TableRow, TableHeader, TableBody, Table } from '@/components/ui/table';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Group } from './groups';
 
-export function GroupsTable({
-  groups,
-}: {
-  groups: any[];
-}) {
+export function GroupsTable({ groups, desc }: { groups: any[]; desc: string}) {
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>群组</CardTitle>
         <CardDescription>
-          我也不知道写点啥，反正先写点副标题
+          {desc}
         </CardDescription>
       </CardHeader>
       <CardContent>
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,17 +3,18 @@ import { GroupsTable } from './groups-table';
 import groups from '@/data/groups.json';
 
 const TAB_CONFIGS = [
-  { value: 'contest', label: '赛事' },
-  { value: 'algo', label: '算法' },
-  { value: 'algo_comp', label: '企业' },
-  { value: 'algo_indie', label: '个人' },
-  { value: 'game', label: '游戏' },
-  { value: 'job', label: '找工' },
-  { value: 'tech', label: '技术' },
-  { value: 'company', label: '公司' },
-  { value: 'city', label: '同城' },
-  { value: 'excited', label: '玩乐' },
-  { value: 'others', label: '其他' }
+  { value: 'contest', label: '赛事', desc: '官方赛事群' },
+  { value: 'algo', label: '算法', desc: '' },
+  { value: 'algo_comp', label: '企业', desc: '企业势算法竞赛交流群' },
+  { value: 'algo_indie', label: '个人', desc: '个人势算法竞赛交流群（个人粉丝群）' },
+  { value: 'game', label: '游戏', desc: '' },
+  { value: 'job', label: '找工', desc: '' },
+  { value: 'tech', label: '技术', desc: '' },
+  { value: 'company', label: '行业', desc: '' },
+  { value: 'city', label: '同城', desc: '' },
+  { value: 'excited', label: '玩乐', desc: '吃喝玩乐' },
+  { value: 'nsfw', label: 'NSFW', desc: 'NSFW' },
+  { value: 'others', label: '其他', desc: '' }
 ];
 
 export default async function MainPage() {
@@ -28,9 +29,9 @@ export default async function MainPage() {
           ))}
         </TabsList>
       </div>
-      {TAB_CONFIGS.map(({ value }) => (
+      {TAB_CONFIGS.map(({ value, desc }) => (
         <TabsContent key={value} value={value}>
-          <GroupsTable groups={groups[value as keyof typeof groups] || []} />
+          <GroupsTable groups={groups[value as keyof typeof groups] || []} desc={desc} />
         </TabsContent>
       ))}
     </Tabs>
